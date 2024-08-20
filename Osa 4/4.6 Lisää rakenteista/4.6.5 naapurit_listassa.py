2968cada7641e440c47168cfa72d23519e4e3e1d
TheZink <ilkka.sinkonen@gmail.com>
2024-08-20 15:05:50 +0300

    20.8.2024

--- Osa 4/4.6 Lisää rakenteista/4.6.5 naapurit_listassa.py
+++ Osa 4/4.6 Lisää rakenteista/4.6.5 naapurit_listassa.py
@@ -1,11 +1,23 @@
-def pisin_naapurijono(lista):
-    new_list = []
-    for i in range(len(lista)):
-        if abs(lista[i-1] - lista[i]) == 1 and i < len(lista)-1:
-            new_list.append(lista[i])
-    return new_list
+def pisin_naapurijono(my_list):
+    longest = 1
+    result = 1
+    for i in range(1, len(my_list)):
+        if abs(my_list[i-1]-my_list[i]) == 1:
+            result += 1
+        else:
+            result = 1
+        longest = max(longest, result)
+            
+            
+    return longest
 
 
 if __name__ == "__main__":
-    lista = [1, 2, 5, 4, 3, 4]
+    lista = [1, 2, 5, 7, 6, 5, 6, 3, 4, 1, 0]
     print(pisin_naapurijono(lista))
+
+
+    #    for i in range(0, len(lista)):
+    #     if abs(lista[i] - lista[i-1]) == 1 or abs(lista[i] - lista[i+1]) == 1:
+    #         new_list.append(lista[i])
+    # return new_list
